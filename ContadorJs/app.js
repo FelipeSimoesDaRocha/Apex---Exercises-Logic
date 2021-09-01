d61b8c1a10c973f984b0e5f953e232c3c767695f
williamtesche <85793944+FelipeSimoesDaRocha@users.noreply.github.com>
2021-09-01 09:50:06 -0300

    Update

---
+++ ContadorJs/app.js
@@ -0,0 +1,23 @@
+
+        function add_field() 
+        {
+            var form = document.getElementsByTagName('form')[0],
+            input = document.createElement('textarea');
+            input.setAttribute('type', 'textarea');
+            input.setAttribute('name', 'item');
+            form.appendChild(input);
+    
+        };
+    
+    
+                    function contador() 
+        {
+            var count = 0;
+            count++;
+    
+            if(count == 10){
+                alert("QWERTY");
+    
+            }
+    
+        };
