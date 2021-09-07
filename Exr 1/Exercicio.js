7ac6fa57d40e5e75a184d1d07cd0d6380206c4f4
williamtesche <85793944+FelipeSimoesDaRocha@users.noreply.github.com>
2021-09-07 17:12:08 -0300

    add new files

---
+++ Exr 1/Exercicio.js
@@ -0,0 +1,12 @@
+function diferenciar() {
+    let numberOne = parseInt(window.prompt("Digite um numero"));
+    let numberTwo = parseInt(window.prompt("Digite outro numero"));
+  
+    if (numberOne > numberTwo) {
+      document.write(numberOne, " é maior que ", numberTwo);
+    } else if (numberOne < numberTwo) {
+      document.write(numberOne, " é menor que ", numberTwo);
+    } else {
+      document.write("os numeros são iguais");
+    }
+  } 
